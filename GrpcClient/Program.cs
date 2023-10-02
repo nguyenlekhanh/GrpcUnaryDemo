28a42d9a93ef8bff74db4b9f394fd3d04832eb9a
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-10-02 11:46:43 -0500

    First example of Grpc service

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System.ComponentModel.DataAnnotations;
+
+namespace GrpcClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //C:\khanh\vs\test\26\1\GrpcUnaryDemo\GrpcService\bin\Debug\net7.0
+            //run service: GrpcService.exe and find the path
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Sample.SampleClient(channel);
+            //var response = client.GetFullName(new SampleRequest { FirstName="John" , LastName="Thomas"});
+            var response = await client.GetFullNameAsync(new SampleRequest { FirstName = "John", LastName = "Thomas" });
+            Console.WriteLine(response.FullName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ GrpcService/Services/SampleService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+
+namespace GrpcService.Services
+{
+    //sample.samplebase get from Protos/Sample.proto line Service Sample
+    public class SampleService : Sample.SampleBase
+    {
+        public override Task<SampleResponse> GetFullName(SampleRequest request, ServerCallContext context)
+        {
+            var result = $"{request.FirstName} {request.LastName}";
+            return Task.FromResult(new SampleResponse { FullName = result });
+        }
+    }
+}
